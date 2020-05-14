44e907bc75ccf60a9be6aa3d4c2ad1162a69ae44
Ahmad9817 <59308632+Ahmad9817@users.noreply.github.com>
2020-05-15 00:44:14 +0100

    Update game.js

--- game.js
+++ game.js
@@ -30,6 +30,7 @@ function checkAnswer(currentLevel) {
 
   else {
     var audio = new Audio("sounds/wrong.mp3");
+    audio.play;
     $("body").addClass("game-over");
     setTimeout(function () {$("body").removeClass("game-over")}, 200)
     $("#level-title").text("Game Over, Press Any Key to Restart");
